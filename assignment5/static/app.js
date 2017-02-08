e2084963c9d735bf173d4b55b0be28490277c4ee
IanCStewart <ian.c.stewart@icloud.com>
2017-02-08 15:23:17 +0100

    :art: Added home call.

--- assignment5/static/app.js
+++ assignment5/static/app.js
@@ -3,6 +3,14 @@
   const section = {
     toggle: function (){
       const route = window.location.hash;
+      if(route === '') {
+        routes.pages.forEach(function (page) {
+          page === 'home'
+          ? document.getElementById(page).classList.remove('invisible')
+          : document.getElementById(page).classList.add('invisible');
+        });
+        return false;
+      }
       routes.pages.forEach(function (page) {
         `#${page}` === route
         ? document.getElementById(page).classList.remove('invisible')
@@ -22,6 +30,7 @@
   const app = {
     init: function (){
       routes.init();
+      section.toggle();
     }
   };
 
